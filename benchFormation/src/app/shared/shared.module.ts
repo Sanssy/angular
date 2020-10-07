f3e2279ac4be2e5bd0351d8e6c946ac9dc1e85f3
sdansso <sanoussy.dansso@aliznet.fr>
2020-10-07 10:23:29 +0200

    directive section

--- benchFormation/src/app/shared/shared.module.ts
+++ benchFormation/src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
 import { DaysPipe } from './pipes/days.pipe';
+import { ColorStateDirective } from './directives/color-state.directive';
 
 
 @NgModule({
-  declarations: [TotalPipe, DaysPipe],
+  declarations: [TotalPipe, DaysPipe, ColorStateDirective],
   imports: [
     CommonModule
   ],
   exports: [
     TotalPipe,
-    DaysPipe
+    DaysPipe,
+    ColorStateDirective
   ]
 })
 export class SharedModule { }
